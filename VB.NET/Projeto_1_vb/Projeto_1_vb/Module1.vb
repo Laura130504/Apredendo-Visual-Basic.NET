96ced3cc5e8031b29d3a8ff35e064add3c595025
Laura Lopes <168434323+Laura130504@users.noreply.github.com>
2026-01-22 19:38:36 -0300

    Add files via upload

---
+++ VB.NET/Projeto_1_vb/Projeto_1_vb/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x As Integer = 1
+        Dim y As Integer = 2
+        Dim z As Integer
+
+        z = y + x
+
+        Console.WriteLine(z)
+
+    End Sub
+
+End Module
